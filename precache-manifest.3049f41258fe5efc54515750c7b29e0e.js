c87da9e4a19508bccf4d09b76f821cb8fb79e202
Najah <najah@najahs-MacBook-Air.local>
2020-03-17 12:54:58 +0300

    Updates

---
+++ precache-manifest.3049f41258fe5efc54515750c7b29e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc98062ab5ec91b39289839bcb28373",
+    "url": "/MonisterApp_react/index.html"
+  },
+  {
+    "revision": "cede64881625ee364439",
+    "url": "/MonisterApp_react/static/css/main.fe56e988.chunk.css"
+  },
+  {
+    "revision": "e6cfe1aece3d0315be28",
+    "url": "/MonisterApp_react/static/js/2.155ac6ca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MonisterApp_react/static/js/2.155ac6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cede64881625ee364439",
+    "url": "/MonisterApp_react/static/js/main.f9c54c04.chunk.js"
+  },
+  {
+    "revision": "a50b412a4c72e2445e52",
+    "url": "/MonisterApp_react/static/js/runtime-main.81175914.js"
+  }
+]);
